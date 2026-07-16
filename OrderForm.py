66d41d1a66ebefee79150f4737b1b77c107ba1c3
SYae01 <syann.ngouela@gmail.com>
2026-07-16 15:30:45 +0200

    Update OrderForm.py

--- OrderForm.py
+++ OrderForm.py
@@ -27,7 +27,8 @@ if ingredient_list:
 
     for ef in ingredient_list:
         ingredients_string += ef +' '
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
+        st.subheader(ef + 'Nutrition Information')
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon" + ef)  
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
     #st.write(ingredients_string)
 
