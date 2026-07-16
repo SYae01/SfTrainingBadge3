f2ec29a59ee73329b50dc8bc8ed9fa1fb1cb60f3
SYae01 <syann.ngouela@gmail.com>
2026-07-16 15:38:17 +0200

    Update OrderForm.py

--- OrderForm.py
+++ OrderForm.py
@@ -14,8 +14,9 @@ st.write('The name on your Smoothie will be:', name_on_order)
 
 cnx = st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-#st.dataframe(data=my_dataframe, use_container_width=True)
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON')
+st.dataframe(data=my_dataframe, use_container_width=True)
+st.stop
 
 ingredient_list = st.multiselect("Choose up to 5 ingredients:"
     , my_dataframe
@@ -27,7 +28,7 @@ if ingredient_list:
 
     for ef in ingredient_list:
         ingredients_string += ef +' '
-        st.subheader(ef + 'Nutrition Information')
+        st.subheader(ef + ' Nutrition Information')
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + ef)  
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
     #st.write(ingredients_string)
