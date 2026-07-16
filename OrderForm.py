36adb409d02cad569bedaa6a72633d8cef4a3076
SYae01 <syann.ngouela@gmail.com>
2026-07-16 15:29:20 +0200

    Update OrderForm.py

--- OrderForm.py
+++ OrderForm.py
@@ -2,7 +2,7 @@
 import streamlit as st
 import os
 from snowflake.snowpark.functions import col
-
+import requests  
 # Write directly to the app
 st.title(f":cup_with_straw: Customize Your Smoothie :cup_with_straw:")
 st.write(
@@ -27,7 +27,8 @@ if ingredient_list:
 
     for ef in ingredient_list:
         ingredients_string += ef +' '
-
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
     #st.write(ingredients_string)
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
@@ -43,7 +44,5 @@ if ingredient_list:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered, ' + name_on_order + '!', icon="✅")
         
-import requests  
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
-# st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+
+
